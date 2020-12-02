9a46bd16f1268530bce63f2859eec3cc2330434c
Michael Taron <mtaron@docugami.com>
2020-12-01 17:10:54 -0800

    - switch to poetry - just use one notebook - add get_feature_names

--- README.md
+++ README.md
@@ -1,15 +1,7 @@
 # sommelier.ai
-A practical workshop on scikit-learn using wine reviews.
+A practical workshop on pandas and scikit-learn using wine reviews.
 
 ## Setup
-1. Install [Miniconda](https://conda.io/miniconda.html)
-2. Open an admin Anaconda Prompt and navigate to this repo
-3. `conda env create -f conda.yml`
-4. `conda activate sommelier`
-
-## Run
-`jupyter lab sommelier.ipynb`
-
-## Cleanup
-1. `conda deactivate`
-2. `conda env remove -n sommelier`
+1. Install [poetry](https://python-poetry.org/docs/#installation)
+1. Run `poetry install` in the root of this repository.
+1. Run `poetry shell` to activate the environment.
--- workshop.py
+++ workshop.py
@@ -1,10 +1,15 @@
-import pandas as pd
 from operator import itemgetter
 
-def boxplot_sorted(df, by, column, **kwds):
+import numpy as np
+import pandas as pd
+from sklearn.pipeline import Pipeline
+
+
+def boxplot_sorted(df: pd.DataFrame, by, column, vert=False, figsize=(10,12), patch_artist=True, **kwds):
     df2 = pd.DataFrame({col:vals[column] for col, vals in df.groupby(by)})
     meds = df2.median().sort_values()
-    df2[meds.index].boxplot(**kwds)
+    df2[meds.index].boxplot(vert=vert, figsize=figsize, patch_artist=patch_artist, **kwds)
+
 
 def show_most_informative_features(model, vectorizer=None, classifier=None, n=20):
     # Source: https://bbengfort.github.io/tutorials/2016/05/19/text-classification-nltk-sckit-learn.html
@@ -21,16 +26,76 @@ def show_most_informative_features(model, vectorizer=None, classifier=None, n=20
 
     # Zip the feature names with the coefs and sort
     coefs = sorted(
-        zip(tvec[0], vectorizer.get_feature_names()),
+        zip(tvec[0], get_feature_names(vectorizer)),
         key=itemgetter(0), reverse=True)
 
     # Get the top n and bottom n coef, name pairs
-    topn  = zip(coefs[:n], coefs[:-(n+1):-1])
+    print("Most Postive")
+    for score, label in coefs[:n]:
+        print(f"{score:.2f}\t{label}")
 
-    output = []
+    print("\nLeast Positive")
+    for score, label in coefs[:-(n+1):-1]:
+        print(f"{score:.2f}\t{label}")
 
-    # Create two columns with most negative and most positive features.
-    for (cp, fnp), (cn, fnn) in topn:
-        output.append("{:0.4f}{: >15}    {:0.4f}{: >15}".format(cp, fnp, cn, fnn))
 
-    print("\n".join(output))
+def get_feature_names(column_transformer):
+    # Source: https://johaupt.github.io/scikit-learn/tutorial/python/data%20processing/ml%20pipeline/model%20interpretation/columnTransformer_feature_names.html
+    """Get feature names from all transformers.
+    Returns
+    -------
+    feature_names : list of strings
+        Names of the features produced by transform.
+    """
+  
+    # Turn loopkup into function for better handling with pipeline later
+    def get_names(trans):
+        # >> Original get_feature_names() method
+        if trans == 'drop' or (
+                hasattr(column, '__len__') and not len(column)):
+            return []
+        if trans == 'passthrough':
+            if hasattr(column_transformer, '_df_columns'):
+                if ((not isinstance(column, slice))
+                        and all(isinstance(col, str) for col in column)):
+                    return column
+                else:
+                    return column_transformer._df_columns[column]
+            else:
+                indices = np.arange(column_transformer._n_features)
+                return ['x%d' % i for i in indices[column]]
+        if not hasattr(trans, 'get_feature_names'):
+        # >>> Change: Return input column names if no method avaiable
+
+            # For transformers without a get_features_names method, use the input
+            # names to the column transformer
+            if column is None:
+                return []
+            else:
+                return [name + "__" + f for f in column]
+
+        return [name + "__" + f for f in trans.get_feature_names()]
+    
+    ### Start of processing
+    feature_names = []
+    
+    # Allow transformers to be pipelines. Pipeline steps are named differently, so preprocessing is needed
+    if type(column_transformer) == Pipeline:
+        l_transformers = [(name, trans, None, None) for step, name, trans in column_transformer._iter()]
+    else:
+        # For column transformers, follow the original method
+        l_transformers = list(column_transformer._iter(fitted=True))
+    
+    
+    for name, trans, column, _ in l_transformers: 
+        if type(trans) == Pipeline:
+            # Recursive call on pipeline
+            _names = get_feature_names(trans)
+            # if pipeline has no transformer that returns names
+            if len(_names)==0:
+                _names = [name + "__" + f for f in column]
+            feature_names.extend(_names)
+        else:
+            feature_names.extend(get_names(trans))
+    
+    return feature_names
